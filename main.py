68468b6b0a8143bf3b0c67265269082756764c2d
Wands77 <wansilvaa200@gmail.com>
2024-12-13 22:35:52 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,17 +3,14 @@ from data.database import inicializar_banco_usuarios, inicializar_banco_tarefas
 from data.sessao import carregar_sessao
 
 def main(page: ft.Page):
-    # Inicializa os bancos de dados
     inicializar_banco_usuarios()
     inicializar_banco_tarefas()
 
-    # Verifica se há uma sessão ativa
     user_id = carregar_sessao()
     if user_id:
         from telas.tela_inicial import main as tela_inicial
-        tela_inicial(page, user_id=user_id)  # Vai direto para a tela inicial
+        tela_inicial(page, user_id=user_id)  
     else:
-        # Exibe a tela de login
         from telas.login import tela_login
         tela_login(page)
 
